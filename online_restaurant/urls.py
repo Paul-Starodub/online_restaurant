618ad482bfb76c8f6484151ab03ddcd901d327bd
Paul Starodub <amorallex@gmail.com>
2023-08-24 21:49:50 +0300

    Updated urls.py with ddt & accounts

--- online_restaurant/urls.py
+++ online_restaurant/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("cuisine/", include("dishes.urls", namespace="cuisine")),
     path("users/", include("users.urls", namespace="users")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
