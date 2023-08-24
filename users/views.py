b7b376499bc22451aea87789793bbcb962d3989b
Paul Starodub <amorallex@gmail.com>
2023-08-24 12:53:57 +0300

    Updated users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from users.forms import CustomerCreationForm
+from users.models import User
+
+
+class CustomerCreateView(generic.CreateView):
+    """Class for creating a new customer"""
+
+    model = User
+    form_class = CustomerCreationForm
+    success_url = reverse_lazy("cuisine:dish-list")
