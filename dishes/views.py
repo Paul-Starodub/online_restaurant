bceb264de3394a6209360c7be6f004bacb7c74a3
Paul Starodub <amorallex@gmail.com>
2023-08-24 09:09:56 +0300

    Updated views.py for endpoint with name types

--- dishes/views.py
+++ dishes/views.py
@@ -1,8 +1,14 @@
 from django.views import generic
 
-from dishes.models import Dish
+from dishes.models import Dish, DishType
 
 
 class DishListView(generic.ListView):
     model = Dish
     template_name = "dishes/dishes_list.html"
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    context_object_name = "dish_type_list"
+    template_name = "dishes/dish_types_list.html"
