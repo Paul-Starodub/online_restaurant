dc1974fc123ca5604000c2456362bd5b489015f5
Paul Starodub <amorallex@gmail.com>
2023-08-24 12:52:47 +0300

    Updated users/models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(_("email address"), unique=True)
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self) -> str:
+        return self.username
