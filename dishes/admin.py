81f080a12cdcc5218647f6a30fca98af96561f76
Paul Starodub <amorallex@gmail.com>
2023-08-26 18:18:32 +0300

    Updated dishes/admin.py

--- dishes/admin.py
+++ dishes/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from dishes.models import DishType, Dish
 
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display_links = ("name",)
+    list_display = ["id", "name", "price", "dish_type"]
+    list_filter = ["dish_type"]
+
+
 admin.site.register(DishType)
-admin.site.register(Dish)
