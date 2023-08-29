38d46b4412fa69d197d87d0afea386a241f3be88
Paul Starodub <amorallex@gmail.com>
2023-08-29 22:59:24 +0300

    Updated blob/urls.py

--- blob/urls.py
+++ blob/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from blob.views import PostListView, PostCreateView
 
-urlpatterns = []
+urlpatterns = [
+    path("", PostListView.as_view(), name="post-list"),
+    path("create/", PostCreateView.as_view(), name="post-create"),
+]
 
 app_name = "posts"
