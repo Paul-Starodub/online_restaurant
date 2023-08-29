29279aaeb84c661dcc51f0cb38e2c3dcd5f7aa71
Paul Starodub <amorallex@gmail.com>
2023-08-29 18:22:08 +0300

    Updated dishes/views.py

--- blob/views.py
+++ blob/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
-# Create your views here.
+from blob.models import Post
+
+
+class PostListView(LoginRequiredMixin, generic.ListView):
+    template_name = "posts/post_list.html"
+    queryset = Post.objects.select_related("user")
+    paginate_by = 2
