26650a0e9548de7d160b942913ee108363f24606
Paul Starodub <amorallex@gmail.com>
2023-08-30 21:30:59 +0300

    Updated blob/urls.py

--- blob/urls.py
+++ blob/urls.py
@@ -6,6 +6,9 @@ from blob.views import (
     PostUpdateView,
     PostDetailView,
     PostDeleteView,
+    CommentaryCreateView,
+    CommentaryDetailView,
+    CommentaryDeleteView,
 )
 
 urlpatterns = [
@@ -14,6 +17,21 @@ urlpatterns = [
     path("<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path(
+        "<int:pk>/commentaries/create/",
+        CommentaryCreateView.as_view(),
+        name="comment-create",
+    ),
+    path(
+        "commentaries/<int:pk>/",
+        CommentaryDetailView.as_view(),
+        name="commentary-detail",
+    ),
+    path(
+        "commentaries/<int:pk>/delete/",
+        CommentaryDeleteView.as_view(),
+        name="commentary-delete",
+    ),
 ]
 
 app_name = "posts"
