41c9af07d8fb9b794025a18ae9b5fc5e7b384165
Paul Starodub <amorallex@gmail.com>
2023-08-30 21:31:49 +0300

    Updated blob/views.py

--- blob/views.py
+++ blob/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
 from blob.forms import PostCustomizeForm
-from blob.models import Post
+from blob.models import Post, Commentary
 
 
 class PostListView(LoginRequiredMixin, generic.ListView):
     template_name = "posts/post_list.html"
-    queryset = Post.objects.select_related("user", "dish")
+    queryset = Post.objects.select_related("dish", "user").prefetch_related(
+        "commentaries"
+    )
     paginate_by = 2
 
 
@@ -22,3 +25,50 @@ class PostCreateView(LoginRequiredMixin, generic.CreateView):
         form.instance.user = self.request.user
         form.save()
         return super().form_valid(form)
+
+
+class PostDetailView(LoginRequiredMixin, generic.DetailView):
+    queryset = Post.objects.select_related(
+        "dish__dish_type", "user"
+    ).prefetch_related("commentaries__user")
+    template_name = "posts/post-detail.html"
+
+
+class PostUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Post
+    fields = ("description", "rating")
+    template_name = "posts/post_form.html"
+    success_url = reverse_lazy("posts:post-list")
+
+
+class PostDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+    template_name = "posts/post_confirm_delete.html"
+    success_url = reverse_lazy("posts:post-list")
+
+
+class CommentaryCreateView(LoginRequiredMixin, generic.CreateView):
+    queryset = Commentary.objects.select_related("post__user")
+    fields = ("content",)
+    template_name = "posts/commentary_form.html"
+    success_url = reverse_lazy("posts:post-list")
+
+    def form_valid(self, form) -> HttpResponseRedirect:
+        form.instance.user = self.request.user
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs["pk"])
+        form.save()
+        return super().form_valid(form)
+
+
+class CommentaryDetailView(LoginRequiredMixin, generic.DetailView):
+    queryset = Commentary.objects.select_related(
+        "post__dish__dish_type", "user"
+    )
+    template_name = "posts/commentary-detail.html"
+    context_object_name = "comment"
+
+
+class CommentaryDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Commentary
+    template_name = "posts/commentary_confirm_delete.html"
+    success_url = reverse_lazy("posts:post-list")
