07312701636d1d406c163ecc92d6671fd6fa931d
Paul Starodub <amorallex@gmail.com>
2023-09-04 16:38:44 +0300

    Updated users/forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 from users.models import User
+from phonenumber_field.formfields import PhoneNumberField
 
 
 class CustomerCreationForm(UserCreationForm):
+    phone = PhoneNumberField(
+        region="UA",
+        widget=PhoneNumberPrefixWidget(
+            country_choices=[
+                ("UA", "Ukraine"),
+            ],
+        ),
+    )
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ("email",)
+        fields = UserCreationForm.Meta.fields + ("email", "phone")
