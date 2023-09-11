76f238297b757fed8a844f1f0fb8f9c4b8027d29
Paul Starodub <amorallex@gmail.com>
2023-09-11 10:52:44 +0300

    Updated users/views.py

--- users/views.py
+++ users/views.py
@@ -1,7 +1,5 @@
-from django.contrib.auth import login
-from django.http import HttpResponseRedirect
-
 from django.views import generic
+
 
 from users.forms import CustomerCreationForm
 from users.models import User
@@ -11,9 +9,4 @@ from django.urls import reverse_lazy
 class CustomerCreateView(generic.CreateView):
     model = User
     form_class = CustomerCreationForm
-    success_url = reverse_lazy("cuisine:dish-list")
-
-    def form_valid(self, form: CustomerCreationForm) -> HttpResponseRedirect:
-        response = super().form_valid(form)
-        login(self.request, self.object)
-        return response
+    success_url = reverse_lazy("users:login")
