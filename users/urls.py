ce595b881e290ea72f80a36cc06c7060a61124d2
Paul Starodub <amorallex@gmail.com>
2023-09-11 10:51:30 +0300

    Updated users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from users.views import CustomerCreateView
 
 urlpatterns = [
     path("create/", CustomerCreateView.as_view(), name="user-create"),
+    path(
+        "login/",
+        LoginView.as_view(),
+        name="login",
+    ),
 ]
 
 app_name = "users"
