6f378faf8392bd94b1fa66c9b28adf82c36c09ce
Paul Starodub <amorallex@gmail.com>
2023-09-12 23:36:05 +0300

    Updated users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from users.views import CustomerCreateView, ProfileView
 
@@ -11,6 +11,9 @@ urlpatterns = [
         name="login",
     ),
     path("profile/", ProfileView.as_view(), name="user-profile"),
+    path(
+        "logout/", LogoutView.as_view(), name="logout"
+    ),  # logout works without this url
 ]
 
 app_name = "users"
