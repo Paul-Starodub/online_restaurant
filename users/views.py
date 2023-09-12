df61427e8933268960b65f466edc0ead8da466aa
Paul Starodub <amorallex@gmail.com>
2023-09-12 20:27:57 +0300

    Updated users/view.py with for_mview

--- users/views.py
+++ users/views.py
@@ -13,24 +13,6 @@ class CustomerCreateView(generic.CreateView):
     success_url = reverse_lazy("users:login")
 
 
-# Example alternative solution(without handling errors)
-# class ProfileView(generic.View):
-#     template_name = "users/profile.html"
-#
-#     def get(self, request, *args, **kwargs):
-#         form = UserProfileForm(instance=request.user)
-#         context = {"form": form}
-#         return render(request, self.template_name, context=context)
-#
-#     def post(self, request, *args, **kwargs):
-#         form = UserProfileForm(request.POST, instance=request.user)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse("users:user-profile"))
-#         context = {"form": form}
-#         return render(request, self.template_name, context=context)
-
-
 class ProfileView(FormView):
     template_name = "users/profile.html"
     form_class = UserProfileForm
