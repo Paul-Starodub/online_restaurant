9ea8adb40799cead3db830703951fdc4f1219ad7
Paul Starodub <amorallex@gmail.com>
2023-09-16 12:40:01 +0300

    Updated settings.py for sending email in console

--- online_restaurant/settings.py
+++ online_restaurant/settings.py
@@ -154,3 +154,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Sending emails
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
