a450b89e3738b6d845432c210d563d25f2f389e5
Paul Starodub <amorallex@gmail.com>
2023-09-16 15:00:37 +0300

    Updated users/admin.py with `EmailVerification`

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import reverse_lazy
 from dishes.admin import BasketAdmin
 
-from users.models import User
+from users.models import User, EmailVerification
 
 admin.site.site_url = reverse_lazy("cuisine:dish-list")
 
@@ -11,3 +11,10 @@ admin.site.site_url = reverse_lazy("cuisine:dish-list")
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "phone", "image")
     inlines = (BasketAdmin,)
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ("code", "user", "expiration")
+    fields = ("code", "user", "expiration", "created")
+    readonly_fields = ("created",)
