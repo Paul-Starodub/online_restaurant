03392cb1e3ca6ebace3d80b313f53d32a789b87b
Paul Starodub <amorallex@gmail.com>
2023-09-18 23:30:50 +0300

    Updated settings.py

--- online_restaurant/settings.py
+++ online_restaurant/settings.py
@@ -158,4 +158,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Sending emails
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
