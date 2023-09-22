67621dbfc5ba7d5dad1a842b81d620c03a514293
Paul Starodub <amorallex@gmail.com>
2023-09-22 14:14:35 +0300

    Updated global urls for social accounts

--- online_restaurant/urls.py
+++ online_restaurant/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("posts/", include("blog.urls", namespace="posts")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
