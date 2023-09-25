7cc74a5896e42c0e5147055d1f2b98af31c48b01
Paul Starodub <amorallex@gmail.com>
2023-09-25 15:11:42 +0300

    Updated blob/tests/test_models.py

--- blog/tests/tests_models.py
+++ blog/tests/tests_models.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from blog.models import Post, Commentary
+
+
+class PostTests(TestCase):
+    fixtures = ["restaurant_data.json"]
+
+    def setUp(self) -> None:
+        self.post = Post.objects.last()
+        self.commentary = Commentary.objects.last()
+
+    def test_post_and_commentary_str(self) -> None:
+        self.assertEqual(str(self.post), self.post.description[:7])
+        self.assertEqual(
+            str(self.commentary), f"posted by {self.commentary.user}"
+        )
+
+    def test_get_absolute_url(self) -> None:
+        url = self.post.get_absolute_url()
+        expected_url = reverse(
+            "posts:post-detail", kwargs={"pk": self.post.pk}
+        )
+
+        self.assertEqual(url, expected_url)
