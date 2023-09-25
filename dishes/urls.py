fa576c8397f101dfa1f1472bcbd68626baf00907
Paul Starodub <amorallex@gmail.com>
2023-09-25 21:39:16 +0300

    Updated settings.py & urls.py with redis cache

--- dishes/urls.py
+++ dishes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from blog.views import PostCreateView
 from dishes.views import (
@@ -20,7 +21,7 @@ from dishes.views import (
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/", cache_page(30)(DishListView.as_view()), name="dish-list"),
     path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path(
         "dishes/<int:pk>/like/",
--- online_restaurant/settings.py
+++ online_restaurant/settings.py
@@ -37,6 +37,12 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
 
 # Application definition
 
