b4f044cf49d92329b25aa9ec781e643d7eb7aec9
Paul Starodub <amorallex@gmail.com>
2023-09-26 22:11:19 +0300

    Added celery.py & tasks.py

---
+++ users/tasks.py
@@ -0,0 +1,19 @@
+import uuid
+from datetime import timedelta
+
+from celery import shared_task
+
+from django.contrib.auth import get_user_model
+from django.utils.timezone import now
+
+from users.models import EmailVerification
+
+
+@shared_task
+def send_email_verification(user_id):
+    user = get_user_model().objects.get(id=user_id)
+    expiration = now() + timedelta(hours=48)
+    record = EmailVerification.objects.create(
+        code=uuid.uuid4(), user=user, expiration=expiration
+    )
+    record.send_verification_email()
