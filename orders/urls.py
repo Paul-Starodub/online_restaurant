7438ce96b0884eb021a87f7b23dd56abe1a93ca4
Paul Starodub <amorallex@gmail.com>
2023-10-04 19:02:02 +0300

    Added orders/urls.py

---
+++ orders/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from orders.views import OrderCreateView
+
+
+urlpatterns = [
+    path("create/", OrderCreateView.as_view(), name="order-create"),
+]
+
+app_name = "orders"
