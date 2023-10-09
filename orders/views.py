a622aeec62cb190297a6061c79a2a21fb1db1e4b
Paul Starodub <amorallex@gmail.com>
2023-10-09 22:42:52 +0300

    Updated orders/views.py

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,14 @@
 import stripe
 
 from django.conf import settings
+from django.contrib.auth import get_user_model, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect, HttpRequest, HttpResponse
+from django.http import (
+    HttpResponseRedirect,
+    HttpRequest,
+    HttpResponse,
+    HttpResponseBadRequest,
+)
 from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView
@@ -19,6 +25,27 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class SuccessTemplateView(TemplateView):
     template_name = "orders/success.html"
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        session_id = request.GET.get("session_id")
+
+        if session_id:
+            # Retrieve information from Stripe session metadata
+            session = stripe.checkout.Session.retrieve(session_id)
+            user_id = session.metadata.get("user_id")
+
+            if user_id:
+                try:
+                    user = get_user_model().objects.get(id=user_id)
+                    user.backend = (
+                        "allauth.account.auth_backends.AuthenticationBackend"
+                    )
+                    login(request, user)
+                except get_user_model().DoesNotExist:
+                    return HttpResponseBadRequest(
+                        "User does not exist", status=HTTPStatus.BAD_REQUEST
+                    )
+        return super().get(request, *args, **kwargs)
 
 
 class CanceledTemplateView(TemplateView):
@@ -37,11 +64,15 @@ class OrderCreateView(LoginRequiredMixin, CreateView):
         baskets = Basket.objects.filter(user=self.request.user)
         checkout_session = stripe.checkout.Session.create(
             line_items=baskets.stripe_products(),
-            metadata={"order_id": self.object.id},
+            metadata={
+                "order_id": self.object.id,
+                "user_id": self.request.user.id,  # save user
+            },
             mode="payment",
             success_url="{}{}".format(
                 settings.DOMAIN_NAME, reverse("orders:order-success")
-            ),
+            )
+            + "?session_id={CHECKOUT_SESSION_ID}",
             cancel_url="{}{}".format(
                 settings.DOMAIN_NAME, reverse("orders:order-canceled")
             ),
@@ -78,11 +109,11 @@ def stripe_webhook_view(request: HttpRequest) -> HttpResponse:
             payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
         )
     except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
+        print(e)
+        return HttpResponse(status=HTTPStatus.BAD_REQUEST)
     except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        return HttpResponse(status=400)
+        print(e)
+        return HttpResponse(status=HTTPStatus.BAD_REQUEST)
 
     # Handle the checkout.session.completed event
     if event["type"] == "checkout.session.completed":
