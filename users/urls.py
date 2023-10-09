62014921da615ec7809f5a0c577a8b6be52ed6b6
Paul Starodub <amorallex@gmail.com>
2023-10-09 14:53:16 +0300

    Updated users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from users.views import (CustomerCreateView, CustomerProfileView,
-                         EmailVerificationView)
+from users.views import (
+    CustomerCreateView,
+    CustomerProfileView,
+    EmailVerificationView,
+)
 
 urlpatterns = [
     path("create/", CustomerCreateView.as_view(), name="user-create"),
@@ -16,7 +19,7 @@ urlpatterns = [
     ),
     path(
         "logout/", LogoutView.as_view(), name="logout"
-    ),  # logout works without this url
+    ),
     path(
         "verify/<str:email>/<uuid:code>/",
         EmailVerificationView.as_view(),
