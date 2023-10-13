76a267b1ddfd22117d8d42f196e907abb76f16f6
Paul Starodub <amorallex@gmail.com>
2023-10-14 00:15:58 +0300

    Updated build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 celery -A online_restaurant worker -l INFO &
+stripe listen --forward-to online-restauraut.onrender.com:8000/webhook/stripe/
