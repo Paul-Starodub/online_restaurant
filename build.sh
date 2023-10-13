01487e952ecc7feef4bb9895d4cf5c50d4f59e45
Paul Starodub <amorallex@gmail.com>
2023-10-14 00:39:53 +0300

    Updated build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-curl -O https://github.com/stripe/stripe-cli/releases/download/v1.7.0/stripe_1.7.0_darwin_amd64.tar.gz
-tar xvf stripe_1.7.0_darwin_amd64.tar.gz
-sudo mv stripe /usr/local/bin/
 stripe listen --forward-to https://online-restauraut.onrender.com:8000/webhook/stripe/ &
 celery -A online_restaurant worker -l INFO &
