3313c23dac42a282dfd28ca4adab70b37f124279
Paul Starodub <amorallex@gmail.com>
2023-10-15 00:14:33 +0300

    Updated global urls.py

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-chmod -R a+w online-restairant/media/uploads/dishes
-chmod -R a+w online-restairant/media/uploads/users
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
