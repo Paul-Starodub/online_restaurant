e2babbca550ad7014921bbffd624f0a3911b022b
Paul Starodub <amorallex@gmail.com>
2023-10-14 22:05:34 +0300

    Updated build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-celery -A online_restaurant worker -l INFO &
+celery --app tasks worker --loglevel info --concurrency 4
