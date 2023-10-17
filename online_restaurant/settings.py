774775630530dd6f39db41e0a1fedc87f12377bf
Paul Starodub <amorallex@gmail.com>
2023-10-17 10:22:05 +0300

    Updated settings.py

--- online_restaurant/settings.py
+++ online_restaurant/settings.py
@@ -175,12 +175,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-if DEBUG:
-    STATICFILES_DIRS = [
-        BASE_DIR / "static",
-    ]
-else:
-    STATIC_ROOT = "staticfiles/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = "staticfiles/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
